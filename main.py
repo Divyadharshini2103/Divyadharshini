b57f91724643b6e483b576d5a6f521c7dfc35af6
Divyadharshini2103 <146007149+Divyadharshini2103@users.noreply.github.com>
2023-10-01 14:16:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-def recur_factorial(n):
-  if n==1:
-    return n
-  else:
-    return n*recur_factorial(n-1)
-num=7
-if num<0:
-  print("Sorry, factorial does not exist for negaive numbers")
-elif num ==0:
+# Python program to find the factorial of a number provided by the user.
+
+num = int(input("Enter a Number:"))
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+  print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
   print("The factorial of 0 is 1")
 else:
-  print("The facorial of",num,"is",recur_factorial(num))
+  for i in range(1, num + 1):
+    factorial = factorial * i
+  print("The factorial of", num, "is", factorial)
